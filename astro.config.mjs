1d16db33580bbb90111b1e0a7adc1b7447597682
Krish Prasad <krishprasad@Krishs-MBP.attlocal.net>
2024-08-20 17:03:05 -0700

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kpkrishprasad.github.io',
-  base: '/Krish-Prasad-Portfolio',
+  base: '/Krish-Prasad-Website',
   integrations: [mdx(), sitemap(), tailwind()]
 });
